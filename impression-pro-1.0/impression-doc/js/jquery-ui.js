887fceb50251cf169dfc42656e76f63ad546fcb6
Rami Rustom <rustom@mit.edu>
2018-10-13 04:11:11 -0400

    reversed changes to impression-pro-1.0

--- impression-pro-1.0/impression-doc/js/jquery-ui.js
+++ impression-pro-1.0/impression-doc/js/jquery-ui.js
@@ -99,7 +99,7 @@ function focusable( element, isTabIndexNotNaN ) {
 		if ( !element.href || !mapName || map.nodeName.toLowerCase() !== "map" ) {
 			return false;
 		}
-		img = $( "images[usemap=#" + mapName + "]" )[0];
+		img = $( "img[usemap=#" + mapName + "]" )[0];
 		return !!img && visible( img );
 	}
 	return ( /input|select|textarea|button|object/.test( nodeName ) ?
@@ -3944,10 +3944,10 @@ $.extend(Datepicker.prototype, {
 			buttonText = this._get(inst, "buttonText");
 			buttonImage = this._get(inst, "buttonImage");
 			inst.trigger = $(this._get(inst, "buttonImageOnly") ?
-				$("<images/>").addClass(this._triggerClass).
+				$("<img/>").addClass(this._triggerClass).
 					attr({ src: buttonImage, alt: buttonText, title: buttonText }) :
 				$("<button type='button'></button>").addClass(this._triggerClass).
-					html(!buttonImage ? buttonText : $("<images/>").attr(
+					html(!buttonImage ? buttonText : $("<img/>").attr(
 					{ src:buttonImage, alt:buttonText, title:buttonText })));
 			input[isRTL ? "before" : "after"](inst.trigger);
 			inst.trigger.click(function() {
