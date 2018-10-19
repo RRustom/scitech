f0be7c86d7309b8e72d0dc4e8210c82c5f2bf7f8
Ihssan <itinawi@mit.edu>
2018-10-19 03:35:29 -0400

    updated landing page of website, added some images

--- pages/urls.py
+++ pages/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('about/', views.AboutPageView.as_view(), name='about'),
     path('contact/', views.AboutPageView.as_view(), name='contact'),
+    path('speakers/', views.SpeakersPageView.as_view(), name='speakers'),
+    path('schedule/', views.SchedulePageView.as_view(), name='schedule'),
+    path('tickets/', views.TicketsPageView.as_view(), name='tickets'),
+    path('blog/', views.BlogPageView.as_view(), name='blog'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -11,3 +11,15 @@ class AboutPageView(TemplateView):
 
 class ContactPageView(TemplateView):
     template_name = 'pages/contact.html'
+
+class SchedulePageView(TemplateView):
+    template_name = 'pages/schedule.html'
+
+class SpeakersPageView(TemplateView):
+    template_name = 'pages/speakers.html'
+
+class BlogPageView(TemplateView):
+    template_name = 'pages/blog.html'
+
+class TicketsPageView(TemplateView):
+    template_name = 'pages/tickets.html'
